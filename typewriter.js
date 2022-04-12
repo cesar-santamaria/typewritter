10459fba894ea3ec8c9e12c78debee18199abf90
Cesar Santamaria <cesar-santamaria@outlook.com>
2022-04-12 17:58:44 -0500

    complete typewritter.js

--- typewriter.js
+++ typewriter.js
@@ -7,3 +7,7 @@ for (const char of sentence) {
     process.stdout.write(char);
   }, delay += 50) // <= 1s delay to make it noticeable. Can dial it down later.
 }
+
+setTimeout(()=>{
+  process.stdout.write('\n')
+},sentence.length * 50)
