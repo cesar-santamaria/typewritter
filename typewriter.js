08e086d32add14a15e9711d76034bb8b972b3ca7
Cesar Santamaria <cesar-santamaria@outlook.com>
2022-04-12 17:52:05 -0500

    add delay to typewritter.js

--- typewriter.js
+++ typewriter.js
@@ -2,9 +2,8 @@ const sentence = "hello there from lighthouse labs";
 let delay = 0;
 
 for (const char of sentence) {
-  process.stdout.write(char); //avoids adding an extra "newline character" each time.
   setTimeout(() => {
     // print the char here
-  },delay += 1000) // <= 1s delay to make it noticeable. Can dial it down later.
+    process.stdout.write(char);
+  }, delay += 50) // <= 1s delay to make it noticeable. Can dial it down later.
 }
-
